464f2dcd756f5c4579247ebe8bb88a8b97356e3a
Josh Zarrabi <jzarrabi@pivotal.io>
2020-04-16 16:34:26 -0400

    remove dead code
    Co-authored-by: Arjun Sreedharan <asreedharan@pivotal.io>

--- cmd/detect/main.go
+++ cmd/detect/main.go
@@ -3,21 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
 
 	"github.com/buildpack/libbuildpack/buildplan"
 	"github.com/cloudfoundry/dotnet-core-aspnet-cnb/aspnet"
 	"github.com/cloudfoundry/libcfbuildpack/detect"
-	"github.com/cloudfoundry/libcfbuildpack/helper"
 )
-
-type BuildpackYAML struct {
-	Config struct {
-		Version string `yaml:"version""`
-	} `yaml:"dotnet-aspnet"`
-}
 
 func main() {
 	context, err := detect.DefaultDetect()
@@ -64,64 +54,4 @@ func runDetect(context detect.Detect) (int, error) {
 	}
 
 	return context.Pass(plan)
-}
-
-func checkIfVersionsAreValid(versionRuntimeConfig, versionBuildpackYAML string) error {
-	splitVersionRuntimeConfig := strings.Split(versionRuntimeConfig, ".")
-	splitVersionBuildpackYAML := strings.Split(versionBuildpackYAML, ".")
-
-	if splitVersionBuildpackYAML[0] != splitVersionRuntimeConfig[0] {
-		return fmt.Errorf("major versions of runtimes do not match between buildpack.yml and runtimeconfig.json")
-	}
-
-	minorBPYAML, err := strconv.Atoi(splitVersionBuildpackYAML[1])
-	if err != nil {
-		return err
-	}
-
-	minorRuntimeConfig, err := strconv.Atoi(splitVersionRuntimeConfig[1])
-	if err != nil {
-		return err
-	}
-
-	if minorBPYAML < minorRuntimeConfig {
-		return fmt.Errorf("the minor version of the runtimeconfig.json is greater than the minor version of the buildpack.yml")
-	}
-
-	return nil
-}
-
-func rollForward(version string, context detect.Detect) (string, bool, error) {
-	splitVersion := strings.Split(version, ".")
-	anyPatch := fmt.Sprintf("%s.%s.*", splitVersion[0], splitVersion[1])
-	anyMinor := fmt.Sprintf("%s.*.*", splitVersion[0])
-
-	versions := []string{version, anyPatch, anyMinor}
-
-	deps, err := context.Buildpack.Dependencies()
-	if err != nil {
-		return "", false, err
-	}
-
-	for _, versionConstraint := range versions {
-		highestVersion, err := deps.Best(aspnet.DotnetAspNet, versionConstraint, context.Stack)
-		if err == nil {
-			return highestVersion.Version.Original(), true, nil
-		}
-	}
-
-	return "", false, fmt.Errorf("no compatible versions found")
-}
-
-func LoadBuildpackYAML(appRoot string) (BuildpackYAML, error) {
-	var err error
-	buildpackYAML := BuildpackYAML{}
-	bpYamlPath := filepath.Join(appRoot, "buildpack.yml")
-
-	if exists, err := helper.FileExists(bpYamlPath); err != nil {
-		return BuildpackYAML{}, err
-	} else if exists {
-		err = helper.ReadBuildpackYaml(bpYamlPath, &buildpackYAML)
-	}
-	return buildpackYAML, err
 }
